b83f50e44b2401cfb1f9d19ef8a00c22e1eb5e3b
Patrick Ostie <patrick.ostie@stroeer.de>
2022-05-06 18:36:25 +0200

    chore: create outputs of relevant resources

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "api" {
+  value = aws_api_gateway_rest_api.api
+  description = "The created api resource."
+}
+
+output "resource-policy" {
+    value = aws_api_gateway_rest_api_policy.api
+    description = "The resource policy of the created api."
+}
